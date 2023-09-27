432327ae20a61bb8936b361b0a2f7bc61207b9ff
Miguel Enrique Alvarez Merlos <malvarezm26@miumg.edu.gt>
2023-09-27 13:29:30 -0600

    replicando el componente

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 // pages/graph.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
 import {
   Chart as ChartJS,
@@ -19,48 +19,81 @@ ChartJS.register(
   PointElement,
   
 );
+const Graph = () => {
+  const [chartData, setChartData] = useState(null);
 
-const Graph = ()=>{
+  useEffect(() => {
+    // Función para calcular los datos de la gráfica
+    const calculateChartData = () => {
+      const labels = [];
+      const datasets = [];
 
-  const data= {
-    labels: ['Mon', 'Tue', 'Wed'],
-    datasets: [
-      {
-        labels: 'Sales of the week',
-        data: [6,3,6],
-        backgroundColor: 'aqua',
-        borderColor: 'black',
-        pointBorderColor: 'aqua',
-        fill: true,
-        tension: 0.4,
+      // Define las funciones para las tres curvas
+      const functions = [
+        {
+          label: "f(x) = x",
+          function: (x) => x,
+          borderColor: "rgba(75, 192, 192, 1)",
+        },
+        {
+          label: "f(x) = x²",
+          function: (x) => x * x,
+          borderColor: "rgba(153, 102, 255, 1)",
+        },
+        {
+          label: "f(x) = x * log(x)",
+          function: (x) => x * Math.log(x),
+          borderColor: "rgba(255, 206, 86, 1)",
+        },
+      ];
+
+      for (let i = 1; i <= 5; i++) {
+        labels.push(i);
       }
-    ]
-  }
 
- const options = {
-  Plugins:{
-    legend: true
-  },
-  scales: {
-    y: {
-      min: 3,
-      max: 6
-    }
-  }
- }
+      functions.forEach((func) => {
+        const data = labels.map((x) => func.function(x));
+        datasets.push({
+          label: func.label,
+          borderColor: func.borderColor,
+          data: data,
+          fill: false,
+        });
+      });
 
-  return(
-    <div className=''>
+      // Configura los datos para el gráfico
+      setChartData({
+        labels: labels,
+        datasets: datasets,
+      });
+    };
+
+    calculateChartData(); // Calcula los datos de la gráfica al cargar la página
+
+  }, []);
+
+  return (
+    <div className="">
       Hello Graph
-      <div className='h-96'>
-      <Line
-      data={data}
-      options={options}
-
-      ></Line>
+      <div className="h-96">
+        {chartData && (
+          <Line
+            data={chartData}
+            options={{
+              scales: {
+                y: {
+                  beginAtZero: true,
+                  ticks: {
+                    maxTicksLimit: 5, // Configura el número máximo de marcas en el eje Y
+                  },
+                },
+              },
+            }}
+          />
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Graph;
